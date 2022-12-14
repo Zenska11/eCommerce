510c25ecf70f03b396dbf58b4193380674ae52bf
Zenska11 <zenska96@gmx.de>
2022-12-14 17:19:45 +0100

    init commit

---
+++ Customer/Customer/Controllers/KafkarProducerController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+[Route("api/kafka")]
+    [ApiController]
+    public class KafkaProducerController : ControllerBase
+    {
+        private readonly ProducerConfig config = new ProducerConfig
+        { BootstrapServers = "localhost:9092" };
+        private readonly string topic = "simple_topic";
+
+        [HttpPost]
+        public IActionResult Post([FromQuery] string message)
+        {
+            return Created(string.Empty, SendToKafka(topic, message));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var product = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "Stift",
+                Color = "Rot",
+                Description = "Bester Stift der Welt"
+            };
+
+            var offering = new Offering
+            {
+                Id = Guid.NewGuid(),
+                Name = "Stift Offering",
+                Quantity = 11,
+                TotalPrice = 10,
+                EffectiveDate = new DateOnly(),
+                Status = "auf lager",
+                Product = product,
+            };
+
+
+            return Ok(offering);
+        }
+
+    private Object SendToKafka(string topic, string message)
+        {
+            using (var producer =
+                    new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    return producer.ProduceAsync(topic, new Message<Null, string> { Value = message })
+                        .GetAwaiter()
+                        .GetResult();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Oops, something went wrong: {e}");
+                }
+            }
+            return null;
+        }
+    }
+
---
+++ Customer/Customer/NewFolder/Product.cs
@@ -0,0 +1,9 @@
+﻿
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Color { get; set; }
+    public string Description { get; set; }
+}
+
